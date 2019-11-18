c40334ce80165d78bee5af57fec18778174ccd88
Livio Assis <livioassis@yahoo.com.br>
2019-11-18 19:53:54 -0300

    acrescentada mensagem de Good Morning

--- testeRepositorioGit/Program.cs
+++ testeRepositorioGit/Program.cs
@@ -7,6 +7,7 @@ namespace testeRepositorioGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Good morning!");
         }
     }
 }
