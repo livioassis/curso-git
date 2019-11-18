09f9ffc9ca069b276ea5b0717c3e6c9a87defddb
Livio Assis <livioassis@yahoo.com.br>
2019-11-18 19:49:33 -0300

    Projeto iniciado, mostrando a mensagem Hello World

---
+++ testeRepositorioGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testeRepositorioGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
